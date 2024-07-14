25d0216b8dd5ed712855aebd658e577a2a68f8bc
Antima1123 <160607543+Antima1123@users.noreply.github.com>
2024-07-14 13:52:01 +0000

    operator

---
+++ Challenge/operator.js
@@ -0,0 +1,90 @@
+//Task 1: Write a program to add two numbers and log the result to the console.
+let a = 10 
+let b = 20
+console.log(a+b)
+console.log(1+2)
+
+//Task 2: Write a program to subtract two numbers and log the result to the console.
+let c = 20 
+let d = 10
+console.log(c-d)
+console.log(2-1)
+
+//Task 3: Write a program to multiply two numbers and log the result to the console.
+let e = 20 
+let f = 10
+console.log(e*f)
+console.log(2*1)
+
+//Task 4: Write a program to divide two numbers and log the result to the console.
+let i = 40 
+let j = 10
+console.log(i/j)
+console.log(4/8)
+
+//Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+
+let k = 80 
+let l = 3
+console.log(k%l)
+console.log(9%2)
+
+//Task 6: Use the operator += to add a number to a variable and log the result to the console.
+let m = 10
+let n = 20
+console.log(m += n)
+
+
+//Task 7: Use the operator -= to subtract a number from a variable and log the result to the console.
+let o = 20
+let p = 10
+console.log(o -= p)
+
+
+//Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+let q = 10
+let r = 20
+console.log(q > r)
+console.log(q < r)
+
+
+//Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+let s = 10
+let t = 20
+console.log(s >= t)
+console.log(s <= t)
+t = 10
+console.log(s >= t)
+console.log(s <= t)
+
+
+//Task 10: Write a program to compare two numbers using "==" and "===" and log the result to the console.
+
+let u = 10
+let v = '10'
+console.log(u === v)
+console.log(u == v)
+
+
+//Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+let var1 = (u == v && u === v)
+console.log(var1)
+
+
+//Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+let var2 = (u == v || u === v)
+console.log(var2)
+
+
+//Task 13: Write a program that uses the! operator to negate a condition and log the result to the console.
+let var3 = (!u == v)
+console.log(var3)
+
+//Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+let var4 = 10 
+let var5 = -1
+let var6 = (var5 >= 0 ? "positive number" : "Negative Number")
+console.log(var6)
+
+
+
